639e2bd37192c11917617f02fbd5e7eb8c7a081d
dhrubb <dhrubbgupta@gmail.com>
2025-07-10 07:40:42 +0530

    Initial commit

---
+++ Week_1/STL.cpp
@@ -0,0 +1,77 @@
+// vector is dynamic array
+// always includes it's library in c++
+
+// array inclusion using STL
+#include <iostream>
+#include <array>
+
+using namespace std;
+int main()
+{
+
+    int basic[3] = {1, 2, 3};
+
+    array<int, 4> a = {1, 2, 3, 4};
+
+    int size = a.size();
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << a[i] << endl;
+    }
+
+    cout << "Element at index 2: " << a.at(2) << endl; // Accessing element at index 2 //output 3
+
+    cout << "Empty or not ->" << a.empty() << endl;  // Checking if the array is empty // output 0 (false)
+    cout << "First element ->" << a.front() << endl; // Accessing the first // element
+    cout << "Last element ->" << a.back() << endl;   // Accessing the last element// output 4
+}
+
+//===================================================
+
+// Vector inclusion using STL
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+
+    vector<int> v;
+    cout << "Capacity->" << v.capacity() << endl; // Output: 0
+
+    v.push_back(1);
+    cout << "Capacity->" << v.capacity() << endl; // Output: 1
+
+    v.push_back(2);
+    cout << "Capacity->" << v.capacity() << endl; // Output: 2
+
+    v.push_back(3);
+    cout << "Capacity->" << v.capacity() << endl; // Output: 4 // Capacity increases to 4 when size exceeds 2, vector doubles its capacity everytime it exceeds the current capacity
+    cout << "Size -> " << v.size() << endl;       // Output: 3
+
+    cout << "before pop" << endl;
+    for (int i : v)
+    {
+        cout << i << " " << endl;
+    }
+
+    v.pop_back(); // Removes the last element
+
+    cout << "after pop" << endl;
+    for (int i : v)
+    {
+        cout << i << " " << endl;
+    }
+
+    // vector clear operation clears all the elements in the vector
+    cout << "Size before clear -> " << v.size() << endl;         // Output: 2
+    cout << "Capacity before clear -> " << v.capacity() << endl; // Output: 4
+    v.clear();                                                   // Clears all elements in the vector
+    cout << "Size after clear -> " << v.size() << endl;          // Output: 0
+    cout << "Capacity after clear -> " << v.capacity() << endl;  // Output: 4
+
+    // size means no of elements in the vector
+    // capacity means no of elements it can hold without reallocation
+    // it is the number of memory blocks allocated for the vector
+    // when we push_back an element and the size exceeds the capacity, the capacity is doubled
+}
