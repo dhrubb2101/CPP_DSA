135d997bd5eb2d0914b82de8c7193ec165b2a5d4
dhrubb <dhrubbgupta@gmail.com>
2025-07-30 19:23:49 +0530

    stl commit testing

--- Week_1/STL.cpp
+++ Week_1/STL.cpp
@@ -37,6 +37,15 @@ int main()
 {
 
     vector<int> v;
+    vector<int> a(5, 1);
+    cout << "print a" << endl;
+    // cout << "print last" << endl;
+    for (int i : a)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
     cout << "Capacity->" << v.capacity() << endl; // Output: 0
 
     v.push_back(1);
@@ -74,4 +83,51 @@ int main()
     // capacity means no of elements it can hold without reallocation
     // it is the number of memory blocks allocated for the vector
     // when we push_back an element and the size exceeds the capacity, the capacity is doubled
+}
+
+//===================================================
+
+// double ended queue inclusion using STL
+// here you can insert and delete elements from both ends
+// it does not have contiguous memory locations like vector and array
+// it is more efficient than vector for insertion and deletion at both ends
+// it is made using multiple fixed static arrays
+// and it is tracked in which array data is being stored
+// it is also dynamic in nature
+
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main()
+{
+    deque<int> d;
+
+    d.push_back(1);  // Inserting at the back
+    d.push_front(2); // Inserting at the front
+
+    for (int i : d)
+    {
+        cout << i << " ";
+    }
+
+    d.pop_back(); // Removing from the back
+    cout << endl;
+    for (int i : d)
+    {
+        cout << i << " ";
+    }
+
+    //     d.pop_front(); // Removing from the front
+    //     cout << endl;
+    //     for (int i : d)
+    //     {
+    //         cout << i << "";
+    //     }
+
+    cout << "Print First Index element -> " << d.at(1) << endl; // Accessing element at index 1
+
+    cout << "front " << d.front() << endl; // Accessing the first element
+    cout << "back " << d.back() << endl;   // Accessing the last element
 }
