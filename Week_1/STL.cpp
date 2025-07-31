12a703c2df6eb63f5183213ecaa97645f0e74cb8
dhrubb <dhrubbgupta@gmail.com>
2025-07-31 22:52:32 +0530

    map inclusion in stl

--- Week_1/STL.cpp
+++ Week_1/STL.cpp
@@ -202,3 +202,199 @@ int main()
 
     cout << "Is stack empty? " << s.empty() << endl; // Output: No
 }
+
+//===================================================
+// queue inclusion using STL
+// it is a FIFO data structure (First In First Out)
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue<string> q;
+
+    q.push("Dhrub");
+    q.push("Rohit");
+    q.push("Rohit2");
+
+    cout << "Front element: " << q.front() << endl; // Output: Dhrub
+
+    q.pop();                                                  // Removes the front element
+    cout << "Front element after pop: " << q.front() << endl; // Output: Rohit
+
+    cout << "Size of queue: " << q.size() << endl; // Output: 2
+
+    cout << "Is queue empty? " << q.empty() << endl; // Output: No
+}
+
+//===================================================
+// priority queue inclusion using STL
+// it is a data structure that stores elements in a way that the highest priority element is always at the front
+// this is max heap by default
+// whatever list we insert, it will always be sorted in descending order
+// if it is min heap, it will be sorted in ascending order, we will get the lowest priority element at the front
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int main()
+{
+    priority_queue<int> pq; // max heap by default
+
+    priority_queue<int, vector<int>, greater<int>> mini; // Uncomment this line for min heap
+
+    pq.push(1);
+    pq.push(3);
+    pq.push(2);
+    pq.push(0);
+
+    cout << "size of priority queue: " << pq.size() << endl; // Output: 4
+    int n = pq.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << pq.top() << "";
+        pq.pop(); // Removes the top element
+    }
+    cout << endl;
+
+    //====================================================
+
+    mini.push(1);
+    mini.push(3);
+    mini.push(2);
+    mini.push(0);
+
+    int n1 = mini.size();
+    for (int i = 0; i < n1; i++)
+    {
+        cout << mini.top() << "";
+        mini.pop(); // Removes the top element
+    }
+    cout << endl;
+
+    //====================================================
+
+    // cout << "Top element: " << pq.top() << endl; // Output: 3
+
+    // pq.pop(); // Removes the top element
+    // cout << "Top element after pop: " << pq.top() << endl; // Output: 2
+
+    // cout << "Size of priority queue: " << pq.size() << endl; // Output: 2
+
+    // cout << "Is priority queue empty? " << pq.empty() << endl; // Output: No
+}
+
+//===================================================
+
+// set inclusion using STL
+// it is a data structure that stores unique elements in a sorted order
+// if you add a duplicate element, it will not be added
+// once an element is added it cannot be modified
+// it is implemented using a balanced binary search tree
+// when you take elements out of the set, they will be in sorted order
+// unordered set is also available which does not store elements in sorted order, they're random
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+int main()
+{
+    set<int> s;
+
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.insert(6);
+
+    cout << "Size of set: " << s.size() << endl; // Output: 6
+
+    for (int i : s)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // Erasing an element
+    s.erase(3); // Removes the element 3
+    cout << "After erasing 3: ";
+    for (int i : s)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // Checking if an element exists
+    if (s.find(2) != s.end())
+        cout << "2 is present in the set" << endl; // Output: 2 is present in the set
+
+    set<int>::iterator it = s.begin();
+    it++;
+
+    s.erase(it); // Erases the second element (which is 2 after erasing 3)
+
+    cout << s.count(4) << endl; // Output: 1 (4 is present in the set) //count tells whether an element is present in the set or not, it returns 1 if present and 0 if not present
+
+    set<int>::iterator itr = s.find(5);
+
+    for (auto it = itr; it != s.end(); it++)
+    {
+        cout << *it << " "; // Output: 5 6 (elements from the iterator itr to the end of the set)
+    }
+    cout << endl;
+
+    cout << "value at iterator itr -> " << *itr << endl; // Output: 5 (the value at the iterator itr)
+}
+
+//===================================================
+
+// map inclusion using STL
+//  it is a data structure that stores key-value pairs
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+int main()
+{
+    map<int, string> m;
+
+    m[1] = "Dhrub";
+    m[2] = "Rohit";
+    m[3] = "Rohit2";
+
+    m.insert({5, "bheem"});
+
+    // Inserting a key-value pair using insert function
+
+    cout << "before erase" << endl;
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl; // Output: key value pairs
+    }
+
+    cout << "finding -13 -> " << m.count(-13) << endl; // Output: 0 (key -13 is not present in the map)
+
+    // m.erase(13);
+
+    cout << "after erase" << endl;
+    m.erase(2); // Erasing the key
+    for (auto i : m)
+    {
+        cout << i.first << " " << i.second << endl; // Output: key value pairs after erasing key 2
+    }
+    cout << endl;
+
+    auto it = m.find(5);
+
+    for (auto i = it; i != m.end(); i++)
+    {
+        cout << (*i).first << endl;
+    }
+}
