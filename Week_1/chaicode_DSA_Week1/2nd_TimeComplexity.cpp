f61bba114a905bd6b6dd7d4448da498ffa50087c
dhrubb <dhrubbgupta@gmail.com>
2025-08-14 14:27:44 +0530

    rate of growth

--- Week_1/chaicode_DSA_Week1/2nd_TimeComplexity.cpp
+++ Week_1/chaicode_DSA_Week1/2nd_TimeComplexity.cpp
@@ -4,5 +4,20 @@
 // -easy to use
 
 // Data structure is divided in two categories
-//  -linear data structure
-//  -non-linear data structure
+//  -linear data structure - array,linked list, stack , queue
+//  -non-linear data structure - tree and graphs
+
+// the difference is 1 can relate with 2 and 2 with 3 and so on. in linear data structure
+// but in non-linear data structure 1 can be connected to 2,3 ,4 or anything else
+
+// -best case - omega notation
+// -average case - theta notation
+// -worst case - big - O notation
+
+// rate of growth
+// linear equation -> y = mx , ax+by+c=0
+// quadratic equation -> ax^2 + bx + c
+// cubic equation ->ax^3 + bx^2 + cx + d
+// bi-quadratic equation - ax^4 + bx^3 + cx^2 + dx + e
+// log equations - alogx + b
+// exponential equation - e^x + 2^3 + 3^x
