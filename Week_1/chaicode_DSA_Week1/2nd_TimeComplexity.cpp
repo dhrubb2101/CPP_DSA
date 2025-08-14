dda1c67ebbb541a1a49174bb0a8ef306cd647e0d
dhrubb <dhrubbgupta@gmail.com>
2025-08-14 13:19:19 +0530

    two types of data strucuture - linear and non-linear

---
+++ Week_1/chaicode_DSA_Week1/2nd_TimeComplexity.cpp
@@ -0,0 +1,8 @@
+// we decide the structure according to three conditions
+// -easy to access
+// -easy to modify
+// -easy to use
+
+// Data structure is divided in two categories
+//  -linear data structure
+//  -non-linear data structure
