f48247d1d142a1024ed345c2169f8e035c13f5d8
dhrubb <dhrubbgupta@gmail.com>
2025-08-24 18:04:18 +0530

    recursion function (how does it work)

--- Week_1/3rd_Recursion.cpp
+++ Week_1/3rd_Recursion.cpp
@@ -1,1 +1,23 @@
-// Function calling itself is recursion
+// Function calling itself is called recursion
+// int main(){
+//     printf("Hello World");
+// }
+// we are calling here one function from one function
+// ek function ke andar se dusre function ko call kra hai
+// yaha kya ho rha hai hello print krna hai ,
+// ek function dusre function ko isliye call krta hai cuz usse kuch kaam karana hota hai
+// jaise yaha main function se kaha hello print kr do toh voh printf function se kehta hai hello print kr do
+// toh yaha ek function dusre function ko call kar raha hai
+
+// and recursion hota hai agr function khud ko he call krne lag jaye toh usse recursion kehte hai
+// for ex
+//  //int fun(){
+//  ----------
+//  ---------
+//  -------
+//  // fun(); //calling fun function inside fun function. //this is called recursion.
+//  ----------
+//  -----
+//  //}
+// here what is happening
+// yaha function khud ko call kar raha hai
